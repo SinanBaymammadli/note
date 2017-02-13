e6b7840769671ddad2c83e81093b0eb2f6a237d3
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2017-02-13 19:33:10 +0400

    adding update functionality and refactoring code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,3 @@
-
-
 initApp = function() {
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (!user) {
@@ -8,38 +6,6 @@ initApp = function() {
 			document.getElementById('app').style.display = 'none';
 
 		} else {
-
-			// User is signed in.
-			const displayName = user.displayName;
-			const email = user.email;
-			const emailVerified = user.emailVerified;
-			const photoURL = user.photoURL;
-			const uid = user.uid;
-			const providerData = user.providerData;
-			const newNoteContent = document.getElementById('new-note');
-			const addNewNoteBtn = document.getElementById('new-note-btn');
-			const body = document.getElementById('body');
-			const notesContainer = document.getElementById('notes');
-			const showNoteBtn = document.getElementsByClassName('note');
-			const noteFullContainer = document.getElementById('note-full-container');
-			const hideNoteBtn = document.getElementById('hide-note-btn');
-			const showAddNoteBtn = document.getElementById('add-note');
-			const addNewNoteContainer = document.getElementById('add-new-note-container');
-			const hideAddNewNoteBtn = document.getElementById('hide-add-new-note');
-			const deleteNoteBtn = document.getElementsByClassName('delete-note');
-			const spinner = document.getElementById('spinner');
-			const noteFullContent = document.getElementById('note-full-content');
-
-			const dbRef = firebase.database();
-			const dbRefNote = dbRef.ref('users/' + uid + '/notes');
-
-
-			addNewNoteBtn.addEventListener('click', addNewNote);
-			hideNoteBtn.addEventListener('click', hideNote);
-			showAddNoteBtn.addEventListener('click', showAddNote);
-			hideAddNewNoteBtn.addEventListener('click', hideAddNewNoteContainer);
-			noteFullContent.addEventListener('dblclick', updateNote);
-
 			user.getToken().then(function(accessToken) {
 
 				document.getElementById('not-logged-in').style.display = 'none';
@@ -55,12 +21,51 @@ initApp = function() {
 
 			});
 
+			// User is signed in.
+			// variables
+			const displayName = user.displayName;
+			const email = user.email;
+			const emailVerified = user.emailVerified;
+			const photoURL = user.photoURL;
+			const uid = user.uid;
+			const providerData = user.providerData;
+
+			const newNoteContent = document.getElementById('new-note');
+			const addNewNoteBtn = document.getElementById('new-note-btn');
+			const body = document.getElementById('body');
+			const notesContainer = document.getElementById('notes');
+			const showNoteBtn = document.getElementsByClassName('note');
+			const noteFullContainer = document.getElementById('note-full-container');
+			const hideNoteBtn = document.getElementById('hide-note-btn');
+			const showAddNoteBtn = document.getElementById('add-note');
+			const addNewNoteContainer = document.getElementById('add-new-note-container');
+			const hideAddNewNoteBtn = document.getElementById('hide-add-new-note');
+			const deleteNoteBtn = document.getElementsByClassName('delete-note');
+			const spinner = document.getElementById('spinner');
+			const noteFullContent = document.getElementById('note-full-content');
+			const saveChangeBtn = document.getElementById('save-change');
+
+			// DB reference
+			const dbRef = firebase.database();
+			const dbRefNote = dbRef.ref('users/' + uid + '/notes');
+
+			// Event listeners
+			addNewNoteBtn.addEventListener('click', function() {
+				addNewNote(newNoteContent);
+			});
+			hideNoteBtn.addEventListener('click', hideFullNoteContainer);
+			showAddNoteBtn.addEventListener('click', showAddNewNoteContainer);
+			hideAddNewNoteBtn.addEventListener('click', hideAddNewNoteContainer);
+			noteFullContent.addEventListener('dblclick', showUpdateNoteContainer);
+
+			// Functions
+			// getting notes from db
 			dbRefNote.on('value', function(snap) {
 
 				notesContainer.innerHTML = "";
 
-				snap.forEach(function (dbNotes) {
-					renderNotes(dbNotes);
+				snap.forEach(function (dbNote) {
+					renderNote(dbNote);
 				});
 
 				hideSpinner();
@@ -69,45 +74,104 @@ initApp = function() {
 
 			});
 
-			function updateNote(note) {
-				console.log('hey');
+			//rendering notes
+			function renderNote(note) {
+				notesContainer.innerHTML += "<div class='card'><span class='delete-note' id='"+ note.key +"'><svg style=width:24px;height:24px' viewBox='0 0 24 24'><path fill='#212121' d='M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z' /></svg></span><div class='note' id='"
+						+note.key+"'><p>"+
+						note.val().content.substring(0,20).replace(/<(?:.|\n)*?>/gm, '')
+						+"</p><p>"+ note.val().created_at +"</p></div></div>";
+			};
+
+			//show spinner
+			function showSpinner() {
+				spinner.style.display = "flex";
 			}
 
-			function addNewNote() {
-				if (newNoteContent.value !== "") {
+			//hide spinner
+			function hideSpinner() {
+				spinner.style.display = "none";
+			};
+
+			//adding click events to each note
+			function addEventListenerToNotes() {
+				for (var i = 0; i < showNoteBtn.length; i++) {
+					showNoteBtn[i].addEventListener('click', function () {
+						showNote(this.id);
+					});
+				}
+			};
+
+			//show note on full page
+			function showNote(noteId) {
+				const noteRef = dbRef.ref('users/' + uid + '/notes/' + noteId);
+				const content = document.createElement('div');
+
+				noteRef.on('value', function(note) {
+					content.innerText = note.val().content;
+					noteFullContent.innerHTML = "";
+					noteFullContent.appendChild(content);
+				});
+
+				noteFullContainer.classList.add('note-full-container-visible');
+				noteFullContent.id = noteId;
+				body.classList.add('overflow-hidden');
+			};
+
+			//adding event listeners to delete buttons
+			function addEventListenerToDeleteNote() {
+				for (var i = 0; i < deleteNoteBtn.length; i++) {
+					deleteNoteBtn[i].addEventListener('click', function (e) {
+						deleteNote(this.id);
+					});
+				}
+			}
+
+			//deleting note
+			function deleteNote(noteId) {
+				if (confirm('Delete this note?'))
+					dbRefNote.child(noteId).remove();
+				else
+					return;
+			};
+
+			// hiding full note container
+			function hideFullNoteContainer() {
+				noteFullContainer.classList.remove('note-full-container-visible');
+				body.classList.remove('overflow-hidden');
+				saveChangeBtn.style.display = "none";
+			};
+
+			// show add new note container
+			function showAddNewNoteContainer() {
+				addNewNoteContainer.classList.add('add-new-note-container-visible');
+				body.classList.add('overflow-hidden');
+			};
+
+			// hide add new note container
+			function hideAddNewNoteContainer() {
+				addNewNoteContainer.classList.remove('add-new-note-container-visible');
+				body.classList.remove('overflow-hidden');
+			};
+
+			// saving new note to database
+			function addNewNote(newNote) {
+
+				if (newNote.value !== "") {
 
 					dbRefNote.push({
-						content: newNoteContent.value,
+						content: newNote.value,
 						created_at: getDate()
 					});
 
 					hideAddNewNoteContainer();
 
-					newNoteContent.value = "";
+					newNote.value = "";
 
-				} else {
+				} else
 					console.log(newNoteContent + " empty value");
-				}
-			}
+			};
 
-			function hideSpinner() {
-				spinner.style.display = "none";
-			}
-
-			function showSpinner() {
-				spinner.style.display = "flex";
-			}
-
-			function deleteNote(note) {
-				const noteId = note.srcElement.id;
-
-				if (confirm('Delete this note?')) {
-					dbRefNote.child(noteId).remove();
-				} else {
-					return;
-				}
-			}
-
+			//get current date
 			function getDate() {
 				var date = new Date();
 				locale = "en-us",
@@ -119,58 +183,44 @@ initApp = function() {
 				return fulldate;
 			}
 
-			function hideAddNewNoteContainer() {
-				addNewNoteContainer.classList.remove('add-new-note-container-visible');
-				body.classList.remove('overflow-hidden');
+			function showUpdateNoteContainer() {
+				const noteId = this.id;
+
+				const textarea = document.createElement('textarea');
+				const currentNoteContent = noteFullContent.innerText;
+				textarea.innerHTML = currentNoteContent;
+				textarea.setAttribute('id', 'updated-note');
+				textarea.setAttribute('autofocus', 'true');
+
+				noteFullContent.innerHTML = "";
+				noteFullContent.appendChild(textarea);
+
+				textarea.focus();
+
+				saveChangeBtn.style.display = "block";
+
+				saveChangeBtn.addEventListener('click', function() {
+					const updateNoteNode = document.getElementById('updated-note');
+
+					if (updateNoteNode) {
+						updatedNote = document.getElementById('updated-note').value;
+					}
+
+					updateNote(updatedNote, noteId);
+				})
 			}
 
-			function showAddNote() {
-				addNewNoteContainer.classList.add('add-new-note-container-visible');
-				body.classList.add('overflow-hidden');
-			};
+			function updateNote(noteContent, noteId) {
+				saveChangeBtn.style.display = "none";
 
-			function renderNotes(notes) {
-				notesContainer.innerHTML += "<div class='card'><span class='delete-note' id='"+ notes.key +"'><svg style=width:24px;height:24px' viewBox='0 0 24 24'><path fill='#212121' d='M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z' /></svg></span><div class='note' id='"
-						+notes.key+"'><p>"+
-						notes.val().content.substring(0,20).replace(/<(?:.|\n)*?>/gm, '')
-						+"</p><p>"+ notes.val().created_at +"</p></div></div>";
-			};
-
-			function addEventListenerToNotes() {
-				for (var i = 0; i < showNoteBtn.length; i++) {
-					showNoteBtn[i].addEventListener('click', function () {
-						showNote(this.id);
-					});
-
-				}
-			};
-
-			function addEventListenerToDeleteNote() {
-				for (var i = 0; i < deleteNoteBtn.length; i++) {
-					deleteNoteBtn[i].addEventListener('click', function (e) {
-						deleteNote(e);
-					});
-
-				}
-			}
-
-			function showNote(noteId) {
 				const noteRef = dbRef.ref('users/' + uid + '/notes/' + noteId);
-
-				noteRef.on('value', function(note) {
-					noteFullContent.innerText = note.val().content;
+				noteRef.set({
+					content: noteContent,
+					created_at: getDate()
 				});
 
-				noteFullContainer.classList.add('note-full-container-visible');
-				body.classList.add('overflow-hidden');
-
-			};
-
-			function hideNote() {
-				noteFullContainer.classList.remove('note-full-container-visible');
-				body.classList.remove('overflow-hidden');
-			};
-
+				showNote(noteId);
+			}
 		}
 
 	}, function(error) {
